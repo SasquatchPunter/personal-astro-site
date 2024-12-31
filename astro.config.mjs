c74043591e348c79309bda7fd1c98330f0066c7d
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-12-31 05:12:08 -0500

    feat: add tailwind and @astrojs/tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
 	devToolbar: {enabled: false},
-	integrations: [react({ experimentalReactChildren: true })],
+	integrations: [react({experimentalReactChildren: true}), tailwind()],
 });
