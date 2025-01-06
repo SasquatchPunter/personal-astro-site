d5f9aac5e505bec8b557992947cb15e985e1b5b5
SasquatchPunter <jeremyrobertelliott@gmail.com>
2025-01-06 13:37:10 -0500

    feat: add sanity client for development

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,10 +1,21 @@
 import { createClient } from "@sanity/client";
 
-const client = createClient({
-	projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-	dataset: "production",
-	useCdn: false,
-	apiVersion: "2025-01-03",
-});
+const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
+const apiVersion = "2025-01-03";
+
+const client = import.meta.env.PROD
+	? createClient({
+			projectId,
+			dataset: "production",
+			useCdn: false,
+			apiVersion,
+		})
+	: createClient({
+			projectId,
+			dataset: "development",
+			useCdn: false,
+			apiVersion,
+			token: import.meta.env.PUBLIC_SANITY_TOKEN,
+		});
 
 export { client };
