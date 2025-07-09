c69dc0555d9bb41266267f5fbeaf12386e44adc0
SasquatchPunter <jeremyrobertelliott@gmail.com>
2025-07-09 01:47:56 -0600

    style: change http-equiv property names in Metadata object

--- src/types.ts
+++ src/types.ts
@@ -2,21 +2,12 @@
  *
  * @example <meta name='keywords' content='your, tags'> == {keywords: "your, tags"}
  */
-export type BasicMetadata = {
+export type Metadata = {
 	abstract?: string;
 	author?: string;
 	description?: string;
 	keywords?: string;
 	robots?: string;
-};
-
-/**
- * Object type to store Http-Equiv meta tag mappings.
- *
- * @example <meta http-equiv='content-security-policy' content='default-src "self"'> == {contentSecurityPolicy: 'default-src "self"'}
- * @link https://www.w3schools.com/tags/att_meta_http_equiv.asp
- */
-export type HttpEquivMetadata = {
-	contentType?: string;
-	refresh?: string;
+	httpContentType?: string;
+	httpRefresh?: string;
 };
